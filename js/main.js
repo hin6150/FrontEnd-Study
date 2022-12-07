e0813e375e53d66014b30f4632599f1431275d0c
hin6150 <hin6150@gmail.com>
2022-12-07 15:07:11 +0900

    V03

--- js/main.js
+++ js/main.js
@@ -14,3 +14,56 @@ searchInputEL.addEventListener("blur", function () {
   searchEL.classList.remove("focused");
   searchInputEL.setAttribute("placeholder", "");
 });
+
+const badgeEL = document.querySelector("header .badges");
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      gsap.to(badgeEL, 0.6, { opacity: 0, display: "none" });
+    } else {
+      gsap.to(badgeEL, 0.6, { opacity: 1, display: "block" });
+    }
+  }, 300)
+);
+
+const fadeELs = document.querySelectorAll(".visual .fade-in");
+fadeELs.forEach(function (fadeEL, index) {
+  gsap.to(fadeEL, 1, { delay: (index + 1) * 0.7, opacity: 1 });
+});
+
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
+
+new Swiper(".promotion .swiper-container", {
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  pagination: {
+    el: ".promotion .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+const promotionEL = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEL.classList.add("hide");
+  } else {
+    promotionEL.classList.remove("hide");
+  }
+});
