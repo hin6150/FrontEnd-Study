30bf63a64c9688eaa0809354504c4983942c8776
hin6150 <hin6150@gmail.com>
2023-02-02 13:31:07 +0900

    New Project

--- codding_apple_react/src/App.js
+++ codding_apple_react/src/App.js
@@ -1,42 +1,14 @@
 import "./App.css";
 
-import { useState } from "react";
 import List from "./components/list";
-/** @jsxImportSource @emotion/react */
-
-import { css } from "@emotion/react";
 
 function App() {
-  const [modal, setModal] = useState(false);
   return (
     <div className="App">
       <div className="title">
-        <h4
-          onClick={() => {
-            setModal(!modal);
-          }}
-        >
-          ReactBlog
-        </h4>
+        <h4>ReactBlog</h4>
       </div>
       <List />
-      {modal ? <Modal /> : null}
-    </div>
-  );
-}
-
-function Modal() {
-  const modalStyle = css`
-    background-color: #eee;
-    padding: 20px;
-    margin-top: 20px;
-    text-align: left;
-  `;
-  return (
-    <div className="modal" css={modalStyle}>
-      <h4>제목</h4>
-      <p>날짜</p>
-      <p>상세 정보</p>
     </div>
   );
 }
--- codding_apple_react/src/components/list.js
+++ codding_apple_react/src/components/list.js
@@ -2,6 +2,31 @@ import React, { useState } from "react";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
+function Modal(props) {
+  const modalStyle = css`
+    background-color: #eee;
+    padding: 20px;
+    margin-top: 20px;
+    text-align: left;
+  `;
+  return (
+    <div className="modal" css={modalStyle}>
+      <h4>{props.listArry[props.index]}</h4>
+      <p>날짜</p>
+      <p>상세 정보</p>
+      <button
+        onClick={() => {
+          let copy = [...props.listArry];
+          copy[0] = "여자코트 추천";
+          props.setListArry(copy);
+        }}
+      >
+        글 수정
+      </button>
+    </div>
+  );
+}
+
 function List() {
   const divStyle = css`
     margin-left: 20px;
@@ -12,16 +37,25 @@ function List() {
     "파이썬 독학",
   ]);
   const [liked, setLiked] = useState([0, 0, 0]);
+  const [modal, setModal] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <div>
       {listArry.map((list, idx) => (
         <div key={idx}>
           <div css={divStyle}>
-            <h4>
+            <h4
+              onClick={() => {
+                setModal(!modal);
+                setIndex(idx);
+              }}
+            >
               {list}
               <span
-                onClick={() => {
+                onClick={(e) => {
+                  e.stopPropagation();
                   liked[idx] += 1;
                   setLiked([...liked]);
                 }}
@@ -31,6 +65,18 @@ function List() {
               {liked[idx]}
             </h4>
             <p>2월 17일 발행</p>
+            <button
+              onClick={() => {
+                let copy = [...listArry];
+                copy.splice(idx, 1);
+                setListArry(copy);
+                let copyLiked = [...liked];
+                copyLiked.splice(idx, 1);
+                setLiked(copyLiked);
+              }}
+            >
+              글 삭제
+            </button>
           </div>
           <hr></hr>
         </div>
@@ -64,6 +110,27 @@ function List() {
       >
         가나다순 정렬하기
       </button>
+      <input
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          let copyList = [...listArry];
+          copyList.push(inputValue);
+          setListArry(copyList);
+
+          let copyLiked = [...liked];
+          copyLiked.push(0);
+          setLiked(copyLiked);
+        }}
+      >
+        글 추가
+      </button>
+      {modal ? (
+        <Modal listArry={listArry} setListArry={setListArry} index={index} />
+      ) : null}
     </div>
   );
 }
