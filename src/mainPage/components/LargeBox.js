35fe8e02e57fb2923fe60a73011eecccd0b7d700
heeeexm <oway0709@naver.com>
2023-07-03 22:41:04 +0900

    메인페이지-버튼 컴포넌트 생성(#1)

--- src/mainPage/components/LargeBox.js
+++ src/mainPage/components/LargeBox.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const LargeBox = () => {
-  return <div>LargeBox</div>;
+  return (
+    <div>
+      <div>image</div>
+      <h1>h1</h1>
+      <p>p</p>
+    </div>
+  );
 };
 
 export default LargeBox;
