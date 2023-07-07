4b943c840cfacd5d6dd0042fb9013f816ea2250a
heeeexm <oway0709@naver.com>
2023-07-08 00:11:05 +0900

    inputbox 수정(#1)

--- src/login/components/InputBox.js
+++ src/login/components/InputBox.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './inputbox.css';
 
 const InputBox = () => {
   return (
-    <div class="box">
-      <div className="rectangle">
-        <img className="image" src="img/smile.png" width="28px" height="28px" alt="" />
-        <input className="content" type="text" placeholder="Sergei Filatov"></input>
+    <div>
+      <div className="account-rectangle">
+        <img className="account-image" src="img/smile.png" width="28px" height="28px" alt="" />
+        <i class="fas fa-search"></i>
+        <input className="account-content" type="text" value="Sergei Filatov"></input>
       </div>
     </div>
   );
