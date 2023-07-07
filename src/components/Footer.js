ebda5373b6bc6f3b8cf6eea315a1a9fe139e22ff
hin6150 <hin6150@gmail.com>
2023-07-07 17:20:46 +0900

    Refactor - Footer 리팩토링 작업(#1)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,57 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
+import './footer.css';
+import { css } from '@emotion/react';
 
 const Footer = () => {
+  const textList = [
+    ['Desktop App', 'Modbile App', 'Sass', 'Event', 'Software'],
+    ['About Us', 'Careers', 'Case Studies', 'Pricing'],
+    ['Blog Listing', 'Blog Article', 'Newsroom'],
+    ['Portfolio', 'Single Case'],
+  ];
   return (
-    <div>
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <div className="box1">
-          <p className="move">Move</p>
-          <br></br>
-          <p className="p">
-            Move - Multipurpose Design<br></br>Template will help you<br></br> develop your website design.
-          </p>
-          <img className="icon1" src="src\assets\Dribbble.png" width="49.69px" height="46.725px" />
-          <img className="icon2" src="src\assets\Facebook.png" width="49.69px" height="46.725px" />
-          <img className="icon3" src="src\assets\Twitter.png" width="49.69px" height="46.725px" />
-          <img className="icon4" src="src\assets\YouTube.png" width="49.69px" height="46.725px" />
-        </div>
-        <div className="box2">
-          <p className="home">Home</p>
-          <p className="desktop">
-            Desktop App<br></br> Mobile App<br></br>SaaS<br></br>Event<br></br>Software<br></br>
-          </p>
-        </div>
-        <div className="box3">
-          <p className="pages">Pages</p>
-          <p className="about">
-            About Us<br></br>Careers<br></br>Case Studies<br></br>Pricing
-          </p>
-        </div>
-        <div className="box4">
-          <p className="blog">Blog</p>
-          <p className="bloglisten">
-            Blog Listing<br></br>Blog Article<br></br>Newsroom
-          </p>
-        </div>
-        <div className="box5">
-          <p className="portfolio">Portfolio</p>
-          <p className="portfo">
-            Portfolio<br></br>Single Case
-          </p>
-        </div>
-      </div>
-      <div className="content">© 2019-2020 Wave Multipurpose Design Template.</div>
+    <div
+      css={css`
+        position: relative;
+        padding: 40px 40px 100px;
+        display: flex;
+        justify-content: space-around;
+      `}
+    >
+      <CategoryLargeBox />
+      <CategoryBox title="Home" data={textList[0]} />
+      <CategoryBox title="Pages" data={textList[1]} />
+      <CategoryBox title="Blog" data={textList[2]} />
+      <CategoryBox title="Portfolio" data={textList[3]} />
     </div>
   );
 };
+const ImageBox = ({ src, width, height }) => <img src={`img/${src}.png`} width={width} height={height} />;
+
+const CategoryLargeBox = () => (
+  <div className="category-large-box">
+    <h2>Move</h2>
+    <p>Move - Multipurpose Design Template will help you develop your website design.</p>
+    <div className="category-icon-box">
+      <ImageBox src="Dribbble" width="50px" height="47px" />
+      <ImageBox src="Facebook" width="50px" height="47px" />
+      <ImageBox src="Twitter" width="50px" height="47px" />
+      <ImageBox src="YouTube" width="50px" height="47px" />
+    </div>
+    <div className="category-copylight">© 2019-2020 Wave Multipurpose Design Template.</div>
+  </div>
+);
+
+const CategoryBox = ({ title, data }) => (
+  <div className="category-box">
+    <h2>{title}</h2>
+    <ul>
+      {data.map((text) => {
+        return <li key={text}>{text}</li>;
+      })}
+    </ul>
+  </div>
+);
 
 export default Footer;
