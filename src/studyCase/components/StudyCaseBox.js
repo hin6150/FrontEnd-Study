511337439574501811c84d6ef19b9aad82d63c42
heeeexm <oway0709@naver.com>
2023-07-09 00:42:15 +0900

    studycasebox 수정(#1)

--- src/studyCase/components/StudyCaseBox.js
+++ src/studyCase/components/StudyCaseBox.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import './studycasebox.css';
 
 const StudyCaseBox = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-      <img className="image" src="img/Image-51.png" width="458px" height="305px"></img>
-      <div className="buttonbox">
-        <button className="button">
-          <img className="image1" src="img/kyan.png" width="100px" height="36px"></img>
-        </button>
+    <div>
+      <div className="casebox-container">
+        <img className="casebox-image" src="img/Image-51.png" width="240px" height="260px" alt="" />
+        <div className="casebox-content">
+          <div className="buttonbox">
+            <button className="casebutton">
+              <img className="casebutton-image" src="img/kyan.png" width="100px" height="36px" alt="" />
+            </button>
+            <h1>Kyan Management</h1>
+            <h2>One of the most important tasks in management is the proper distribution of responsibilities.</h2>
+          </div>
+        </div>
       </div>
-      <br></br>
-      <p className="kyan">Kyan Management</p>
-      <br></br>
-      <p className="content">
-        One of the most important tasks<br></br> in management is the proper<br></br> distribution of responsibilities.
-      </p>
     </div>
   );
 };
