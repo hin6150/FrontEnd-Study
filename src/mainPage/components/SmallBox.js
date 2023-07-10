48417fa0069ab3c578991342a96bbe7a4a4aa7b7
heeeexm <oway0709@naver.com>
2023-07-10 15:55:07 +0900

    smallbox 수정(#1)

--- src/mainPage/components/SmallBox.js
+++ src/mainPage/components/SmallBox.js
@@ -4,14 +4,20 @@ import { Image } from '../../components/components';
 
 const SmallBox = () => {
   return (
-    <div className="small-box">
-      <div className="icon">
-        <Image src="icon-2" width="80px" height="80px" />
+    <div className="small-box-container">
+      <div className="small-box">
+        <div className="icon">
+          <Image src="icon-2" width="80px" height="80px" />
+        </div>
+        <div className="text">
+          <span>Notification</span>
+          <h2>
+            Try the messenger <br />
+            features!
+          </h2>
+        </div>
       </div>
-      <div className="text">
-        <span>Notification</span>
-        <h2>Try the messenger features!</h2>
-      </div>
+      <img className="smallbox-image" alt="" src="img/Image-18.png" width="600px" height="500px" />
     </div>
   );
 };
