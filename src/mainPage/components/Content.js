14452b1333b1ffb28ad052ae964be815cad56289
heeeexm <oway0709@naver.com>
2023-07-10 15:55:35 +0900

    content 생성(#1)

---
+++ src/mainPage/components/Content.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './content.css';
+
+const content = () => {
+  return (
+    <div>
+      <h1>Try the familiar functions in the new execution</h1>
+      <h2>
+        The very functions that help you in everyday life are already built into our messenger and are not just built in
+        but invented anew.
+      </h2>
+      <button className="content-button">Get Started</button>;
+    </div>
+  );
+};
+
+export default Content;
