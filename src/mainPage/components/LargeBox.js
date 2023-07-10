ae1b52f2c5c28efb5fe96b925118a7753909ec0d
heeeexm <oway0709@naver.com>
2023-07-10 15:54:54 +0900

    largebox 수정(#1)

--- src/mainPage/components/LargeBox.js
+++ src/mainPage/components/LargeBox.js
@@ -5,7 +5,7 @@ const LargeBox = () => {
   return (
     <div className="main-block">
       <div className="icon-wrapper">
-        <img className="icon-image" alt="Nothing" src="img/icon-6.png" width="392px" height="135px"></img>
+        <img className="icon-image" alt="" src="img/icon-6.png" width="392px" height="135px"></img>
       </div>
       <h1>End-to-End</h1>
       <h2>Our messenger uses only proven technologies. There fore, no one will read your messages.</h2>
